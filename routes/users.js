4f4ab1ca20d53c09a94cc44a02e7b42d53ce68b3
Skuiggly <piplup107@gmail.com>
2022-06-17 11:33:01 +0200

    feat: add uplifting message in /users/cool/

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+/* Challenge yourself :) */
+/* cool */
+router.get('/cool/', (req, res, next) => {
+  res.send('You\'re so cool')
+})
 module.exports = router;
